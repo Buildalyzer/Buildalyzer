938ded42708dc0d029587884962289a0cdccc3df
Corniel Nobel <corniel@gmail.com>
2026-04-18 12:18:13 +0200

    Dispose when needed

--- src/Buildalyzer/Extensions/System.Collections.IEnumerable.cs
+++ src/Buildalyzer/Extensions/System.Collections.IEnumerable.cs
@@ -4,6 +4,23 @@ internal static class EnumerableExtensions
 {
     extension(IEnumerable enumerable)
     {
-        public bool HasAny => enumerable.GetEnumerator().MoveNext();
+        public bool HasAny
+        {
+            get
+            {
+                var enumerator = enumerable.GetEnumerator();
+                try
+                {
+                    return enumerator.MoveNext();
+                }
+                finally
+                {
+                    if (enumerator is IDisposable disposable)
+                    {
+                        disposable.Dispose();
+                    }
+                }
+            }
+        }
     }
 }
