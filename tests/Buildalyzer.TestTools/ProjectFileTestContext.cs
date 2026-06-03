c9de5cf9130eb4668ea340e98819df4f96d58de2
Corniel Nobel <corniel@gmail.com>
2026-06-03 14:57:44 +0200

    Separate TestTools project, so that unit tests projects do not have to depend on each other.

--- tests/Buildalyzer.TestTools/ProjectFileTestContext.cs
+++ tests/Buildalyzer.TestTools/ProjectFileTestContext.cs
@@ -1,6 +1,3 @@
-using System.Diagnostics;
-using System.IO;
-
 namespace Buildalyzer.TestTools;
 
 /// <summary>Creates a test context for testing <see cref="IProjectAnalyzer"/>s.</summary>
---
+++ tests/Buildalyzer.TestTools/Properties/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using System;
+global using System.Diagnostics;
+global using System.Diagnostics.Contracts;
+global using System.IO;
